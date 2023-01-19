09ccc7d5a4cdb85dd820b9213596c02d06cd96c6
matthewgrossman <matthewgrossman@localhost>
2023-01-19 12:01:46 -0600

    Messages! pls read and respond ASAP

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -3,6 +3,6 @@ package studio0;
 public class Message {
 
 	public static void main(String args[]) {
-		System.out.println("I love burritos!");
+		System.out.println("Do you guys want to get lunch at Stanley's after studio");
 	}
 }
